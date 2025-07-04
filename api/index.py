7ab4c2b2c1f6ac9437d00de45b56f86de9a44f08
alexfinch7 <atf325@nyu.edu>
2025-07-03 23:04:23 -0400

    changes

--- api/index.py
+++ api/index.py
@@ -1,15 +1,7 @@
-# api/index.py
-
-# Restrict exports so Vercel only sees `handler`
 __all__ = ["handler"]
 
 def handler(event, context):
-    """
-    Entrypoint for Vercel’s Python runtime.
-    Translates Lambda-style events into WSGI requests for Flask.
-    """
     import awsgi
     from webapp import app
 
-    # awsgi.response wraps your Flask app in a Lambda-compatible response
     return awsgi.response(app, event, context)
