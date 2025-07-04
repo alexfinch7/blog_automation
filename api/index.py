b47d738d614812b8189c83e5d9e7cbfdd1312b21
alexfinch7 <atf325@nyu.edu>
2025-07-03 22:46:53 -0400

    Define handler function in index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,10 @@
-from webapp import app as handler 
+import awsgi
+from webapp import app
+
+def handler(event, context):
+    """
+    This is the entrypoint Vercel will call.
+    awsgi.response will translate the Lambda event+context
+    into a WSGI request for Flask, and back into a Lambda response.
+    """
+    return awsgi.response(app, event, context)
