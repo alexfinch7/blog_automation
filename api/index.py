21f8bcf16fb8046c63585a1986c8e64ff1f41b69
alexfinch7 <atf325@nyu.edu>
2025-07-03 22:55:21 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,17 +1,1 @@
-# api/index.py
-
-# Tell Python/Vercel “__all__ only contains handler”
-__all__ = ["handler"]
-
-def handler(event, context):
-    """
-    Vercel will invoke this.  We import AWGI
-    and your Flask `app` here so that no other
-    symbols escape into module scope.
-    """
-    import awsgi
-    from webapp import app
-
-    # Turn the Lambda-style event/context into a WSGI request,
-    # dispatch it to your Flask app, and return the response.
-    return awsgi.response(app, event, context)
+from server import app
